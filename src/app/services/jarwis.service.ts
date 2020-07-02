478a60aefaba1b2bd11262792c83eb27a510c73a
Nguyenhieuanh <nguyenhieuanh.061626@gmail.com>
2020-07-02 22:07:08 +0700

    create login method in jarwis service

--- src/app/services/jarwis.service.ts
+++ src/app/services/jarwis.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class JarwisService {
+  private baseUrl: 'http://awesomequiz.com/api';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(data) {
+    return this.http.post(`${this.baseUrl}/login`, data);
+  }
 }
